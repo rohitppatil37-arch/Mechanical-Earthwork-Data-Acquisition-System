cc3bf5cb39314d1d112430f4c7c9866f555165c9
rohitppatil37-arch <rohitppatil37@gmail.com>
2026-03-02 13:52:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "PASTE_YOUR_EXEC_URL_HERE";
+const API_URL = "https://script.google.com/macros/s/AKfycbzOhvYrDFrXe6vo7hjeMkuFH0GJ2exdLNfzy9ZyPASiCu-JcmXY7yHTxixNq29tACB5Jg/exec";
 
 let CONFIG = {};
 
